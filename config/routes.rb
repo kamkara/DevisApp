306d130bedf99621336c75d298fd9433330c993a
kamkara <lnclass.kamkara@gmail.com>
2022-08-23 17:45:57 +0000

    Add turbo drive

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root to:'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to:'quotes#index'
+
+  resources :quotes
+  
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
