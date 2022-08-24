9bf487a27b6e2765a0688ec537cec29ab66ca774
kamkara <lnclass.kamkara@gmail.com>
2022-08-24 15:04:34 +0000

    Add LineItem Date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,19 @@ module ApplicationHelper
         end
     end
 
+    #Flash Messages
     def render_turbo_stream_flash_messages
         turbo_stream.prepend "flash", partial: "layouts/flash"
     end
+
+
+    #Errors Notices
+    def form_error_notification(object)
+    if object.errors.any?
+      tag.div class: "error-message" do
+        object.errors.full_messages.to_sentence.capitalize
+      end
+    end
+  end
+
 end
