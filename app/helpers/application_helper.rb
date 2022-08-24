1951d3220a075230147abbfbaabc583b7afcf44a
kamkara <lnclass.kamkara@gmail.com>
2022-08-24 16:46:20 +0000

    Add nested_dom_id from helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,9 @@ module ApplicationHelper
     end
   end
 
+  #Nested dom_id
+  def nested_dom_id(*args)
+    args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join("_")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   
 
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
   
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
